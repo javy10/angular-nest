e4475ff199126358ecdb7869e718cff84eadea98
Lawliet <49668391+alramirez97@users.noreply.github.com>
2021-07-09 15:44:19 -0600

    Interfaces y ruta

--- aerolinea/src/app/app-routing.module.ts
+++ aerolinea/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReservacionListComponent } from './reservacion/reservacion-list/reservacion-list.component'; 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    component: ReservacionListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
